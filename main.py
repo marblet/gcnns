849705feb5cf78c3331518b64e52bf581b2c815f
marble0117 <guitarmarble0117@gmail.com>
2019-11-22 18:17:31 +0900

    run the model

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from data.data import load_data
+from models.gcn import create_gcn_model
+from train import run
+from utils import preprocess_features
 
-a = load_data("cora")
+
+if __name__ == '__main__':
+    data = load_data('cora')
+    data.features = preprocess_features(data.features)
+    model, optimizer = create_gcn_model(data)
+    run(data, model, optimizer)
