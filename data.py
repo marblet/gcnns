67797da86cb2a18839e75b976b901dd524e82c56
marble0117 <guitarmarble0117@gmail.com>
2019-11-22 10:36:07 +0900

    data loader

--- data.py
+++ data.py
@@ -1,25 +1,44 @@
 import networkx as nx
+import numpy as np
 import pickle as pkl
 import sys
 import torch
-from itertools import repeat
+
+
+class Data(object):
+    def __init__(self, adj, features, labels, train_mask, val_mask, test_mask):
+        self.adj = adj
+        self.features = features
+        self.labels = labels
+        self.train_mask = train_mask
+        self.val_mask = val_mask
+        self.test_mask = test_mask
+
+    def to(self, device):
+        return self.apply(lambda x: x.to(device))
 
 
 def load_data(dataset_str):
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
-    for i in range(len(names)):
-        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+    for name in names:
+        with open("data/ind.{}.{}".format(dataset_str, name), 'rb') as f:
             if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
+                out = pkl.load(f, encoding='latin1')
             else:
-                objects.append(pkl.load(f))
+                out = objects.append(pkl.load(f))
+
+            if name == 'graph':
+                objects.append(out)
+            else:
+                out = out.todense() if hasattr(out, 'todense') else out
+                objects.append(torch.Tensor(out))
 
     x, y, tx, ty, allx, ally, graph = tuple(objects)
     test_idx = parse_index_file("data/ind.{}.test.index".format(dataset_str))
     train_idx = torch.arange(y.size(0), dtype=torch.long)
     val_idx = torch.arange(y.size(0), y.size(0) + 500, dtype=torch.long)
-    sorted_test_idx = test_idx.sort()[0]
+    sorted_test_idx = np.sort(test_idx)
 
     if dataset_str == 'citeseer':
         len_test_idx = (test_idx.max() - test_idx.min()).item() + 1
@@ -42,13 +61,18 @@ def load_data(dataset_str):
     val_mask = index_to_mask(val_idx, labels.shape[0])
     test_mask = index_to_mask(test_idx, labels.shape[0])
 
-    return adj, features, labels, train_mask, val_mask, test_mask
+    data = Data(adj, features, labels, train_mask, val_mask, test_mask)
+
+    return data
 
 
 def adj_mat_from_dict(graph):
     G = nx.from_dict_of_lists(graph)
-    adj = G
-    return adj
+    coo_adj = nx.to_scipy_sparse_matrix(G).tocoo()
+    indices = torch.from_numpy(np.vstack((coo_adj.row, coo_adj.col)).astype(np.int64))
+    values = torch.from_numpy(coo_adj.data)
+    shape = torch.Size(coo_adj.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
 
 
 def index_to_mask(index, size):
