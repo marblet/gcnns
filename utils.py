34759674dbc1f11c9136b3c72f7453f9da1478e3
marble0117 <guitarmarble0117@gmail.com>
2019-11-22 18:16:37 +0900

    preprocessing function

---
+++ utils.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def add_self_loops(edge_list, size):
+    i = torch.arange(size).view(1, -1)
+    self_loops = torch.cat((i, i), dim=0)
+    edge_list = torch.cat((edge_list, self_loops), dim=1)
+    return edge_list
+
+
+def normalize_adj(edge_list):
+    row, col = edge_list
+    deg = torch.bincount(row)
+    deg_inv_sqrt = torch.pow(deg.to(torch.float), -0.5)
+    deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0.0
+    weight = torch.ones(edge_list.size(1))
+    v = deg_inv_sqrt[row] * weight * deg_inv_sqrt[col]
+    norm_adj = torch.sparse.FloatTensor(edge_list, v)
+    return norm_adj
+
+
+def preprocess_features(features):
+    features = features / features.sum(dim=1, keepdim=True)
+    return features
