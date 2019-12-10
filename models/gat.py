95ed7dd96de7c1339d95de951aebde15f016465e
marble0117 <guitarmarble0117@gmail.com>
2019-12-10 18:12:21 +0900

    leaky, init value, dropout

--- models/gat.py
+++ models/gat.py
@@ -38,9 +38,8 @@ class GATConv(Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        print('gat conv parameters reset')
         nn.init.xavier_uniform_(self.fc.weight, gain=1.414)
-        nn.init.xavier_uniform_(self.att.weight)
+        nn.init.xavier_uniform_(self.att.weight, gain=1.414)
         if self.fc.bias is not None:
             self.fc.bias.data.fill_(0)
 
@@ -50,11 +49,12 @@ class GATConv(Module):
         x = self.fc(x)
         wh_cat = torch.cat((x[source], x[target]), dim=1)
         wh_cat = torch.unsqueeze(wh_cat, 1)
-        attention = F.leaky_relu(self.att(wh_cat))
+        attention = F.leaky_relu(self.att(wh_cat), negative_slope=self.alpha)
         attention = torch.squeeze(attention)
         adj = torch.sparse.FloatTensor(edge_list, attention).to_dense()
         adj[adj == 0] = -9e15
         adj = F.softmax(adj, dim=1)
+        adj = F.dropout(adj, p=self.dropout, training=self.training)
         x = torch.matmul(adj, x)
         return x
 
