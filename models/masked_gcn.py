c97dd220df4a65a1772c70f4c1733fedca49118e
marble_tsubame <taguchi.h.ae@m.titech.ac.jp>
2020-01-29 15:17:44 +0900

    for cuda

--- models/masked_gcn.py
+++ models/masked_gcn.py
@@ -48,7 +48,7 @@ class MaskedGCNConv(Module):
         self.in_features = in_features
         self.out_features = out_features
         self.fc = nn.Linear(in_features, out_features, bias=bias)
-        self.degree = get_degree(data.edge_list).float()
+        self.degree = get_degree(data.edge_list).float().to(device)
         self.dense_adj = data.adj.to_dense().to(device)
         self.sigma = Parameter(torch.Tensor(in_features))
         self.reset_parameters()
