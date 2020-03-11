0dc0da92fdc4d837bbeb794c3f4cff0fac98780d
marble0117 <guitarmarble0117@gmail.com>
2020-03-11 12:57:54 +0900

    update main

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from data.data import load_data
 from models.gcn import create_gcn_model
 from models.gat import create_gat_model
 from models.sgc import create_sgc_model
 from models.gfnn import create_gfnn_model
 from models.masked_gcn import create_masked_gcn_model
+from models.appnp import create_appnp_model
+from models.ppnp import create_ppnp_model
+from data.data import load_data
 from train import run
 from utils import preprocess_features
 
+import random
+import numpy as np
+import torch
+
+SEED = 17
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
 
 if __name__ == '__main__':
     data = load_data('cora')
     data.features = preprocess_features(data.features)
-    model, optimizer = create_gat_model(data)
-    run(data, model, optimizer, epochs=100000, patience=100, niter=5, use_acc=True, save_model=True)
+    model = create_gcn_model(data)
+    run(data, model, lr=0.005, weight_decay=5e-4, epochs=200, patience=10, niter=10)
