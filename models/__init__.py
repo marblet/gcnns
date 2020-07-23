8d0518ea90c60bb03f5d3589a7d31903023cdc14
marblet <guitarmarble0117@gmail.com>
2020-07-23 13:22:12 +0900

    update masked gcn

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@ from .appnp import APPNP
 from .gat import GAT
 from .gcn import GCN
 from .gfnn import GFNN
-from .masked_gcn import MaskedGCN
+from .masked_gcn import MaskedGCNSym, MaskedGCNAsym
 from .mixhop import MixHop
 from .ppnp import PPNP
 from .sgc import SGC
--- models/masked_gcn.py
+++ models/masked_gcn.py
@@ -2,65 +2,69 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
-from torch.nn.modules.module import Module
 
 from utils import get_degree
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def make_mask(x, edge_list, dense_adj, sigma, deg):
-    source, target = edge_list
-    h = torch.pow((x[source] - x[target]), 2)
-    h = h * dense_adj[source, target].view(-1, 1)
-    mask = torch.zeros(x.size(), device=device)
-    mask.index_add_(0, source, h)
-    mask = mask / (sigma * sigma)
-    mask = torch.exp(- mask / deg.view(-1, 1))
-    return mask
-
-
-class MaskedGCN(nn.Module):
-    def __init__(self, data, nhid=32, dropout=0.7):
-        super(MaskedGCN, self).__init__()
-        nfeat, nclass = data.num_features, data.num_classes
-        self.gc1 = MaskedGCNConv(nfeat, nhid, data)
-        self.gc2 = MaskedGCNConv(nhid, nclass, data)
+class MaskedGCNSym(nn.Module):
+    def __init__(self, data, dropout=0.5):
+        super(MaskedGCNSym, self).__init__()
+        self.fc = nn.Linear(data.num_features, data.num_classes, bias=False)
+        self.degree = get_degree(data.edge_list).float().to(device)
+        self.dense_adj = data.adj.to_dense().to(device)
+        self.sigma = Parameter(torch.FloatTensor(data.num_features))
         self.dropout = dropout
 
     def reset_parameters(self):
-        self.gc1.reset_parameters()
-        self.gc2.reset_parameters()
+        self.fc.reset_parameters()
+        self.sigma.data = torch.ones_like(self.sigma) * 100
+
+    def calc_edge_weight(self, x, edge_list):
+        source, target = edge_list
+        diff = torch.pow((x[source] - x[target]), 2)
+        diff = diff / (self.sigma * self.sigma)
+        edge_weight = torch.exp(- torch.sum(diff, dim=1))
+        return edge_weight
+
+    def generate_mask(self, x, edge_list, edge_weight):
+        source, target = edge_list
+        diff = torch.pow((x[source] - x[target]), 2)
+        diff = edge_weight.unsqueeze(1) * diff
+        mask = torch.zeros(x.size(), device=device)
+        mask.index_add_(0, source, diff)
+        mask = mask / (self.sigma * self.sigma)
+        mask = torch.exp(- mask / self.degree.unsqueeze(1))
+        return mask
+
+    def update(self, x, edge_list, edge_weight):
+        feature_mask = self.generate_mask(x, edge_list, edge_weight)
+        x = x * feature_mask
+        source, target = edge_list
+        weight_sum = torch.zeros(x.size(0), device=device)
+        weight_sum.index_add_(0, source, edge_weight)
+        edge_weight = edge_weight / weight_sum[source]
+        h = torch.zeros_like(x)
+        h.index_add_(0, source, edge_weight.unsqueeze(1) * x[target])
+        return h
 
     def forward(self, data):
-        x = data.features
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = F.relu(self.gc1(x, data))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.gc2(x, data)
+        x, edge_list = data.features, data.edge_list
+        edge_weight = self.calc_edge_weight(x, edge_list)
+        x = self.update(x, edge_list, edge_weight)
+        x = self.update(x, edge_list, edge_weight)
+        x = self.fc(x)
         return F.log_softmax(x, dim=1)
 
 
-class MaskedGCNConv(Module):
-    def __init__(self, in_features, out_features, data, bias=True):
-        super(MaskedGCNConv, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.fc = nn.Linear(in_features, out_features, bias=bias)
-        self.degree = get_degree(data.edge_list).float().to(device)
-        self.dense_adj = data.adj.to_dense().to(device)
-        self.sigma = Parameter(torch.Tensor(in_features))
-        self.reset_parameters()
+class MaskedGCNAsym(MaskedGCNSym):
+    def __init__(self, data, dropout=0.5):
+        super(MaskedGCNAsym, self).__init__(data, dropout)
+        self.b = nn.Parameter(torch.FloatTensor(2 * data.num_features))
 
-    def reset_parameters(self):
-        nn.init.xavier_uniform_(self.fc.weight)
-        if self.fc.bias is not None:
-            self.fc.bias.data.fill_(0)
-        self.sigma.data.fill_(1)
-
-    def forward(self, x, data):
-        mask = make_mask(x, data.edge_list, self.dense_adj, self.sigma, self.degree)
-        x = mask * x
-        x = self.fc(x)
-        x = torch.spmm(data.adj, x)
-        return x
+    def calc_edge_weight(self, x, edge_list):
+        source, target = edge_list
+        x_cat = torch.cat((x[source], x[target]), dim=1)
+        edge_weight = torch.exp(torch.sum(self.b * x_cat, dim=1))
+        return edge_weight
